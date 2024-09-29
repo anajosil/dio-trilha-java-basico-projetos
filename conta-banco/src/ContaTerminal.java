42302f283938fa44563576076e1c6293000e337c
anasabarreto <ajsbm80@gmail.com>
2024-09-29 08:46:27 -0300

    desafio conta bancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+// import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Por favor, digite o número da sua conta");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+
+        System.out.println("Por favor, digite o saldo");
+        double saldo = scanner.nextDouble();
+
+
+       
+        System.out.println("Olá, " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco. A sua agência é " + agencia + ", conta " + conta + ", e seu saldo de " + saldo + " já está disponível para saque.");
+
+
     }
+
+
+
 }
